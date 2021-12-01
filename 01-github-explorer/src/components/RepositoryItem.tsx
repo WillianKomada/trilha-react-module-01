d593e9188736ab44945850820d1f0928120c6eee
Willian Komada <willian10komada@hotmail.com>
2021-12-01 17:28:12 -0300

    styles and adjustments

--- 01-github-explorer/src/components/RepositoryItem.tsx
+++ 01-github-explorer/src/components/RepositoryItem.tsx
@@ -4,15 +4,29 @@ interface RepositoryItemProps {
     description: string;
     html_url: string;
   };
+  // batata?: {
+  //   name: string;
+  //   amount: string;
+  // };
 }
 
-export function RepositoryItem(props: RepositoryItemProps) {
+// Devo utilizar props quando não quero especificar um elemento do interface
+
+/* 
+  Example: props.repository.name                  || repository.name
+           objetoCompleto.específico.propriedade
+           props.batata?.name
+           objetoCompleto.específico.propriedade   || batata?.name
+
+*/
+
+export function RepositoryItem({ repository }: RepositoryItemProps) {
   return (
     <li>
-      <strong>{props.repository.name}</strong>
-      <p>{props.repository.description}</p>
+      <strong>{repository.name}</strong>
+      <p>{repository.description}</p>
 
-      <a href={props.repository.html_url} target="_blank">
+      <a href={repository.html_url} target="_blank">
         Acessar repositório
       </a>
     </li>
--- 01-github-explorer/src/components/RepositoryList.tsx
+++ 01-github-explorer/src/components/RepositoryList.tsx
@@ -13,22 +13,36 @@ export function RepositoryList() {
   const [repositories, setRepositories] = useState<Repository[]>([]);
 
   useEffect(() => {
-    fetch("https://api.github.com/orgs/rocketseat/repos") // quando eu fizer uma chamada a api
+    fetch("https://api.github.com/users/WillianKomada/repos") // quando eu fizer uma chamada a api
       .then((response) => response.json()) // eu recebo a resposta e converto pra json
       .then((data) => setRepositories(data)); // quando a resposta pra json terminar de ser convertida, terei meus dados do repositório
   }, []);
 
   return (
-    <section className="repository-list">
-      <h1>Lista de repositórios</h1>
+    <div className="repository-container">
+      <section className="repository-list">
+        <div className="repository-header">
+          <div className="card-h1">
+            <h1>Meus repositórios</h1>
+          </div>
+          <div className="card-github">
+            <span>
+              Mais informações:
+              <a href="https://github.com/williankomada" target="_blank">
+                GitHub
+              </a>
+            </span>
+          </div>
+        </div>
 
-      <ul>
-        {repositories.map((repository) => {
-          return (
-            <RepositoryItem key={repository.name} repository={repository} />
-          );
-        })}
-      </ul>
-    </section>
+        <ul>
+          {repositories.map((repository) => {
+            return (
+              <RepositoryItem key={repository.name} repository={repository} />
+            );
+          })}
+        </ul>
+      </section>
+    </div>
   );
 }
